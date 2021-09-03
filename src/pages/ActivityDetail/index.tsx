f03d859e2d6c71316585d00397c6b54f8f27e76d
Sobraloser <Asgoth55@gmail.com>
2021-09-02 22:16:49 -0300

    feat: add ActivityDetails page

---
+++ src/pages/ActivityDetail/index.tsx
@@ -0,0 +1,219 @@
+import { useEffect, useMemo, useState, useCallback } from 'react'
+import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion'
+import { useHistory } from 'react-router-dom'
+import { FiHeadphones, FiTrash, FiCheck, FiBook } from 'react-icons/fi'
+
+import { BottomMenu } from '../../components/BottomMenu'
+import { Header } from '../../components/header'
+import { LoadingStatus } from '../../components/LoadingStatus'
+import { Player } from '../../components/Player'
+import { useLoading } from '../../contexts/LoadingContext'
+
+// import Medic from '../../images/medic.svg'
+// import Clock from '../../images/clock.svg'
+// import Gym from '../../images/Gym.svg'
+// import Games from '../../images/games.svg'
+// import Food from '../../images/food.svg'
+// import Respiration from '../../images/respiration.svg'
+// import Meditation from '../../images/meditation.svg'
+
+import { api } from '../../services/api'
+import { useActivity } from '../../contexts/ActivityContext'
+import { Modal } from '../../components/Modal'
+
+import styles from './styles.module.scss'
+
+// const icon = {
+//   Musica: <FiHeadphones size={30} color="#fff"/>,
+//   Exercicios: <img src={Gym} alt="" style={{ width: 30, height: 30 }}/>,
+//   Games : <img src={Games} alt="" style={{ width: 30, height: 30 }}/>,
+//   Meditacao: <img src={Meditation} alt="" style={{ width: 30, height: 30 }}/>,
+//   Culinaria: <img src={Food} alt="" style={{ width: 30, height: 30 }}/>,
+//   Respiracao: <img src={Respiration} alt="" style={{ width: 30, height: 30 }}/>,
+//   Estudos: <FiBook size={30} color="#fff"/>,
+//   medic : <img src={Medic} alt="" style={{ width: 30, height: 30 }}/>,
+//   clock : <img src={Clock} alt="" style={{ width: 30, height: 30 }}/>
+// }
+
+export const ActivityDetails: React.FC = () => {
+  const [ isVisible, setIsVisible ] = useState(false)
+  const [ isModalSuccessVisible, setIsModalSuccessVisible ] = useState(false)
+  const [ isModalRemoveVisible, setIsModalRemoveVisible ] = useState(false)
+  const { isLoading, setLoadingTrue, closeLoading } = useLoading()
+  const { selectedActivity } = useActivity()
+  
+  const history = useHistory()
+
+  useEffect(()=> { setIsVisible(true) },[])
+  useEffect(()=> { closeLoading() },[ closeLoading ])
+  useEffect(() => {
+    if(!selectedActivity) {
+      history.push('/Activities')
+    }
+  },[selectedActivity, history])
+
+  const Finish = useCallback(() => {
+    setLoadingTrue()
+    setIsModalSuccessVisible(true)
+  },[setLoadingTrue, setIsModalSuccessVisible])
+
+  const ExcludeActivity = useCallback(async () => {
+    setLoadingTrue()
+    setIsModalRemoveVisible(false)
+    setTimeout(() => history.push('/activities'), 300)
+  },[setLoadingTrue, setIsModalRemoveVisible, history])
+  
+  const MemoizedModalExclude = useMemo(()=>(
+    <AnimatePresence exitBeforeEnter>
+      { isModalRemoveVisible && (
+        <Modal
+          title="Oh não..."
+          description="Você tem certeza de que deseja excluir essa tarefa?"
+          keyModal="Exclude"
+          isVisible={isModalRemoveVisible}
+          setIsVisible={setIsModalRemoveVisible}
+          yesAndNoButtons={true}
+          confirmFunction={ExcludeActivity}
+          image="Exclude"
+          destinyPage="Activity/Activities"
+        />
+      ) }
+      
+    </AnimatePresence>
+  ),[isModalRemoveVisible, ExcludeActivity])
+
+  const MemoizedModalSuccess = useMemo(()=>(
+    <Modal
+      title="Parabéns!"
+      description="Você conseguiu realizar uma tarefa, isso é ótimo!"
+      keyModal="Success"
+      isVisible={isModalSuccessVisible}
+      setIsVisible={setIsModalSuccessVisible}
+      yesAndNoButtons={false}
+      image="Success"
+      destinyPage="Activities"
+    />
+  ),[isModalSuccessVisible])
+
+  const memoizedDetails = useMemo(()=>(
+    <>
+      <div className={styles.activityItem}>
+        <div className={styles.icon}>
+          {/* {icon[String(selectedActivity?.category.name)]} */}
+        </div>
+
+        <div className={styles.content}>
+          <h2>{selectedActivity?.title}</h2>
+          <p>{selectedActivity?.description}</p>
+        </div>
+      </div>
+
+      <div 
+        className={styles.ActivityDescription} 
+        dangerouslySetInnerHTML={{
+          __html: selectedActivity?.description as string
+        }}
+      >
+      </div>
+    </>
+  ),[selectedActivity])
+
+  const memoizedHeader = useMemo(()=>(
+    <Header GoBackIsActive={true}/>
+  ),[])
+
+  const memoizedBottomMenu = useMemo(()=>(
+    <BottomMenu pageActive='activities'/>
+  ),[])
+
+  const memoizedButtonsControl = useMemo(()=>(
+    <div className={styles.buttons}>
+      <button type="button" onClick={() => setIsModalRemoveVisible(true)}>
+        Descartar
+      </button>
+
+      <button type="button" onClick={Finish}>
+        Concluir
+      </button>
+    </div>
+  ),[Finish, setIsModalRemoveVisible])
+
+  return(
+    <div className={styles.container}>
+        {memoizedHeader}
+
+        {isModalSuccessVisible && (MemoizedModalSuccess)}
+
+        {isModalRemoveVisible && (MemoizedModalExclude)}
+
+        <AnimateSharedLayout type="crossfade">
+         
+          <AnimatePresence exitBeforeEnter>
+            {isVisible && (
+              
+              <motion.main
+              layout
+              key="ActivityDetails"
+              initial={{ opacity: 0, height: 0, y: 50 }}
+              animate={{ opacity: 1, height: "fit-content", y: 0}}
+              exit={{ opacity: 0, height: 0}}
+              >
+                {isLoading && (<LoadingStatus/>) }
+                
+                {memoizedDetails}
+
+                {selectedActivity?.files.map(item => {
+                
+                  if(item.format === "mp4"){
+                    return(
+                      <motion.video 
+                        controls 
+                        className={styles.video} 
+                        key={item.id}
+                        initial={{ opacity: 0}}
+                        animate={{ opacity: 1}}
+                      >
+                        <source src={item.url} type="video/mp4"/>
+                        Your browser does not support the video tag.
+                      </motion.video>
+                    )
+                  }
+        
+                  if(item.format === "mp3"){
+                    return(
+                      <>
+                        <strong>Nós recomendamos essa música</strong>
+                        <Player 
+                          name={item.name || ''}
+                          url={item.url || ''}
+                          duration={item.duration || 0}
+                        />
+                      </>
+                    )
+                  }
+        
+                  if(item.format === "png"){
+                    return(
+                      <div className={styles.image} key={item.id}>
+                        <img 
+                          key={item.id}
+                          src={item.url} 
+                          alt="Imagem do arquivo" 
+                          width={512}
+                          height={288}
+                      />
+                      </div>
+                      
+                    )
+                  }
+                  return <p key={item.id}>banana</p>
+                })}
+                {memoizedButtonsControl}
+              </motion.main>
+            )}
+          </AnimatePresence>
+          {memoizedBottomMenu}
+        </AnimateSharedLayout>
+    </div>
+  )
+}

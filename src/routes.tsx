8c9aaf73573d8af9db394e474a4c9c241f06b6d6
Sobraloser <Asgoth55@gmail.com>
2021-09-02 22:15:37 -0300

    feat: update routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,11 @@ import { IonReactRouter } from '@ionic/react-router';
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
 import { Questionnaire } from './pages/Questionnaire';
+import { Home } from './pages/Home';
+import { Clock } from './pages/Clock';
+import { Profile } from './pages/Profile';
+import { Activities } from './pages/Activities';
+import { ActivityDetails } from './pages/ActivityDetail';
 
 export const Routes: React.FC = () => {
   return(
@@ -18,6 +23,11 @@ export const Routes: React.FC = () => {
         <Route exact path="/SignIn" component={SignIn} />
         <Route exact path="/SignUp" component={SignUp} />
         <Route exact path="/Questionnaire" component={Questionnaire} />
+        <Route exact path="/Home" component={Home} />
+        <Route exact path="/Clock" component={Clock} />
+        <Route exact path="/Profile" component={Profile} />
+        <Route exact path="/Activities" component={Activities} />
+        <Route exact path="/ActivityDetails" component={ActivityDetails} />
 
       </IonRouterOutlet>
     </IonReactRouter>

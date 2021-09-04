df764fa6f034557d8fa95b7a2e528d7bb3664194
Sobraloser <Asgoth55@gmail.com>
2021-09-03 22:42:33 -0300

    feat: turn profile dinamic

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useHistory, Link } from 'react-router-dom';
-import { AnimatePresence, motion, useTransform, useViewportScroll } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { FiBook, FiLock, FiLogOut, FiSettings } from 'react-icons/fi';
 // import { format } from 'date-fns'
 
@@ -10,42 +10,39 @@ import { LoadingStatus } from '../../components/LoadingStatus';
 import { Modal } from '../../components/Modal'
 
 import { useLoading } from '../../contexts/LoadingContext';
-import { removeToken } from '../../utils/handleToken';
 
 import styles from './styles.module.scss'
+import { useUsers } from '../../contexts/UserContext';
 
 export const Profile: React.FC = () => {
   const history = useHistory()
-  
   const [ isVisible, setIsVisible ] = useState(false)
   const { isLoading, closeLoading, setLoadingTrue } = useLoading()
   const [ settingsIsVisible, setSettingsIsVisible ] = useState(false)
   const [ isLogoutModalVisible, setIsLogoutModalVisible ] = useState(false)
-  const [ user, setUser ] = useState()
-  
-  const { scrollYProgress } = useViewportScroll()
-  const progress = useTransform(scrollYProgress, [0, 1], [1, 0.5]);
+  const { Logout, user } = useUsers()
+
+  const logout = useCallback(() => {
+    Logout().then(() => { history.push('/SignIn') })
+  },[history, Logout])
  
   useEffect(()=>{ setIsVisible(true) },[])
   useEffect(() => { closeLoading() },[ closeLoading ])
 
-  const Logout =  useCallback(() => {
-    removeToken()
-    history.push("/SignIn")
-    return
-  },[history])
-
   const memoizedModalLogout = useMemo(()=>(
-    <Modal
-      title={`Volte sempre ${': )'}`}
-      description="Você tem certeza de que deseja sair do nosso app?"
-      keyModal="Logout"
-      isVisible={isLogoutModalVisible}
-      setIsVisible={setIsLogoutModalVisible}
-      yesAndNoButtons={true}
-      confirmFunction={Logout}
-    />
-  ),[isLogoutModalVisible, Logout])
+    <AnimatePresence exitBeforeEnter>
+      { isLogoutModalVisible && (
+        <Modal
+          title={`Volte sempre ${': )'}`}
+          description="Você tem certeza de que deseja sair do nosso app?"
+          keyModal="Logout"
+          setIsVisible={setIsLogoutModalVisible}
+          yesAndNoButtons={true}
+          confirmFunction={logout}
+        />
+      ) }
+    </AnimatePresence>
+  ),[isLogoutModalVisible, logout])
 
   const memoizedHeader = useMemo(()=>(
     <Header GoBackIsActive/>
@@ -54,20 +51,20 @@ export const Profile: React.FC = () => {
   const memoizedHeaderUser = useMemo(()=>(
     <section className={styles.headerUserContainer}>
       <div className={styles.ImageAndNameContainer}>
-        <img src="https://github.com/GabrSobral.png" alt="Minha foto de perfil" />
-        <span>Gabriel Sobral dos Santos</span>
+        <img src="https://github.com/c-santana4.png" alt="Minha foto de perfil" />
+        <span>{user?.name}</span>
       </div>
 
-      <span className={styles.registeredAt}>Registrado em: 23/08/2020</span>
+      <span className={styles.registeredAt}>Registrado em: {user?.created_at}</span>
     </section>
-  ),[])
+  ),[user?.name, user?.created_at])
 
   const memoizedAllActivities = useMemo(()=>(
     <div className={styles.allActivitiesComplete}>
       <span>Atividades completas:</span>
-      <span>5</span>
+      <span>{user?.all_activities_finished}</span>
     </div>
-  ),[])
+  ),[user?.all_activities_finished])
 
   const memoizedSettingsButton = useMemo(()=>(
     <div className={styles.configurationsContainer}>

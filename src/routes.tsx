016dbc3ba9507679c0a7866faa7b080c66263ef4
Sobraloser <Asgoth55@gmail.com>
2021-09-07 09:27:20 -0300

    feat: add redirect and private routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,3 +1,4 @@
+import { createElement } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
@@ -10,8 +11,22 @@ import { Clock } from './pages/Clock';
 import { Profile } from './pages/Profile';
 import { Activities } from './pages/Activities';
 import { ActivityDetails } from './pages/ActivityDetail';
+import { useUsers } from './contexts/UserContext';
 
 export const Routes: React.FC = () => {
+  const { isAuthenticated, user } = useUsers()
+
+  const PrivateRoute = ({component, ...rest}: any) => {
+    const routeComponent = (props: any) => (
+      (isAuthenticated && user)
+        ? (user.hasAnswered ? 
+          createElement(component, props) : 
+          <Redirect to={{ pathname: "/Questionnaire", state: { from: props.location } }}/>)
+        : <Redirect to={{ pathname: "/SignIn", state: { from: props.location } }} />
+    )
+    return <Route {...rest} render={routeComponent}/>;
+  }
+
   return(
     <IonReactRouter>
       <IonRouterOutlet>
@@ -20,14 +35,25 @@ export const Routes: React.FC = () => {
           <Redirect to="/SignIn" />
         </Route>
 
-        <Route exact path="/SignIn" component={SignIn} />
-        <Route exact path="/SignUp" component={SignUp} />
-        <Route exact path="/Questionnaire" component={Questionnaire} />
-        <Route exact path="/Home" component={Home} />
-        <Route exact path="/Clock" component={Clock} />
-        <Route exact path="/Profile" component={Profile} />
-        <Route exact path="/Activities" component={Activities} />
-        <Route exact path="/ActivityDetails" component={ActivityDetails} />
+        <Route 
+          exact path="/SignIn" 
+          render={() => (isAuthenticated && user) ? 
+            <Redirect to={'/Home'}/> : 
+            <SignIn/>}
+        />
+        <Route 
+          exact path="/SignUp" 
+          render={() => (isAuthenticated && user) ? 
+            <Redirect to={'/Home'}/> : 
+            <SignUp/>}
+        />
+
+        <Route exact path="/Questionnaire" component={ Questionnaire} />
+        <PrivateRoute exact path="/Home" component={Home} />
+        <PrivateRoute exact path="/Clock" component={Clock} />
+        <PrivateRoute exact path="/Profile" component={Profile} />
+        <PrivateRoute exact path="/Activities" component={Activities} />
+        <PrivateRoute exact path="/ActivityDetails" component={ActivityDetails} />
 
       </IonRouterOutlet>
     </IonReactRouter>

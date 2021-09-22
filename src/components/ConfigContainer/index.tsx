5fcf4d9941f361d8e7ff40ae3ad7e1e5e9352007
Sobraloser <Asgoth55@gmail.com>
2021-09-22 16:00:48 -0300

    refact: uncouple configContainer from Profile

---
+++ src/components/ConfigContainer/index.tsx
@@ -0,0 +1,69 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import { useState } from 'react'
+import { FiSettings, FiBook, FiLock, FiBookOpen } from 'react-icons/fi'
+import { useHistory } from 'react-router'
+import styles from './styles.module.scss'
+
+interface ConfigContainerProps {
+  setIsVisibleToFalse: () => void;
+  setQuantityModalToVisible: () => void;
+}
+
+export function ConfigContainer({
+   setIsVisibleToFalse, setQuantityModalToVisible }: ConfigContainerProps){
+  const [ isSettingVisible, setIsSettingtVisible ] = useState(false)
+  const history = useHistory()
+
+  return(
+    <div className={styles.configurationsContainer}>
+      <button 
+        type="button" 
+        className={styles.configurationButton} 
+        onClick={()=> setIsSettingtVisible(!isSettingVisible)}
+      >
+        Configurações
+        <FiSettings size={27} color="#534A4A"/>
+      </button>
+
+      <AnimatePresence>
+      {isSettingVisible && (
+        <motion.div 
+          key="SettingsButton"
+          className={styles.hiddenButtons}
+          initial={{ height: 0 }}
+          animate={{ height: "fit-content"}}
+          exit={{ height: 0 }}
+        >
+          <button 
+            type="button"
+            onClick={() => {
+              setIsVisibleToFalse()
+              setTimeout(() => history.push('/Questionnaire'),300)
+            }}
+          >
+            Alterar questionário
+            <FiBook size={20} color="#6f6b6b"/>
+          </button>
+          
+          <button
+            type="button"
+            onClick={() => {
+              setIsVisibleToFalse()
+              setTimeout(() => history.push('/ChangePassword'),300)
+            }}
+          >
+            Alterar senha
+            <FiLock size={20} color="#6f6b6b"/>
+          </button>
+
+          <button type="button" onClick={setQuantityModalToVisible}>
+            Quantidade de atividades diárias
+            <FiBookOpen size={20} color="#6f6b6b"/>
+          </button>
+
+        </motion.div>
+      )}
+      </AnimatePresence>
+    </div>
+  )
+}

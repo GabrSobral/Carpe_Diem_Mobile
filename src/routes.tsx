848fa0a78d0c77640c52c81ad112189e42d9090f
Sobraloser <Asgoth55@gmail.com>
2021-09-23 10:07:29 -0300

    feat: add tab bar to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,10 @@
 import { createElement } from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { IonRouterOutlet } from '@ionic/react';
+import { IonRouterOutlet, IonTabs, IonTabBar, IonTabButton } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { FiList, FiUser, FiHome} from 'react-icons/fi'
+
+import styles from './styles/routes.module.scss'
 
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
@@ -41,26 +44,48 @@ export const Routes: React.FC = () => {
         <Route 
           exact path="/SignIn" 
           render={() => (user) ? 
-            <Redirect to={'/Home'}/> : 
+            <Redirect to={'/tabs/Home'}/> : 
             <SignIn/>}
         />
         <Route 
           exact path="/SignUp" 
           render={() => (user) ? 
-            <Redirect to={'/Home'}/> : 
+            <Redirect to={'/tabs/Home'}/> : 
             <SignUp/>}
         />
-
-        <Route exact path="/Questionnaire" component={ Questionnaire} />
-        <PrivateRoute exact path="/Home" component={Home} />
-        <PrivateRoute exact path="/Clock" component={Clock} />
-        <PrivateRoute exact path="/Profile" component={Profile} />
-        <PrivateRoute exact path="/Activities" component={Activities} />
-        <PrivateRoute exact path="/ActivityDetails" component={ActivityDetails} />
-        <PrivateRoute exact path="/ChangePassword" component={ChangePassword} />
+        <Route exact path="/Questionnaire" component={Questionnaire} />
         <Route exact path="/ForgotPassword" component={ForgotPassword} />
         <Route exact path="/ResetPassword" component={ResetPassword} />
+        <PrivateRoute exact path="/Clock" component={Clock} />
+        <PrivateRoute exact path="/ChangePassword" component={ChangePassword} />
 
+        <Route
+          path="/tabs"
+          render={() => (
+            <IonTabs>
+              <IonRouterOutlet>
+                <Route exact path="/tabs/Home" component={Home} />
+                <Route exact path="/tabs/Profile" component={Profile} />
+                <Route exact path="/tabs/Activities" component={Activities} />
+                <Route exact path="/tabs/ActivityDetails" component={ActivityDetails} />
+              </IonRouterOutlet>
+
+              <IonTabBar slot="bottom" className={styles.container}>
+                <IonTabButton tab="activities" href="/tabs/Activities">
+                  <FiList className={styles.iconButton}/>
+                </IonTabButton>
+
+                <IonTabButton tab="home" href="/tabs/Home">
+                  <FiHome className={styles.iconButton}/>
+                </IonTabButton>
+
+                <IonTabButton tab="profile" href="/tabs/Profile">
+                  <FiUser className={styles.iconButton}/>
+                </IonTabButton>
+              </IonTabBar>
+            </IonTabs>
+          )}
+        />
       </IonRouterOutlet>
     </IonReactRouter>
   )

18b6a4040906b978f36c849528759c4ec9372080
Sobraloser <Asgoth55@gmail.com>
2021-09-23 10:08:14 -0300

    feat: add isPlatform and status-bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { IonApp } from '@ionic/react';
+import { IonApp, isPlatform } from '@ionic/react';
+import { StatusBar, Style } from '@capacitor/status-bar'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -8,6 +9,13 @@ import { UserProvider } from './contexts/UserContext';
 import { ActivityProvider } from './contexts/ActivityContext';
 
 const App: React.FC = () => {
+  if(isPlatform('mobile')){
+    (async () => {
+      StatusBar.setOverlaysWebView({ overlay: true });
+      await StatusBar.setStyle({ style: Style.Light })
+    })()
+  }
+
   return(
     <IonApp>
       <UserProvider>
